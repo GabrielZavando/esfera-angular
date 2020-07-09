beee43f71fe933c521a25ced66d15302de950f37
Gabriel Zavando <gabrielzavando@gmail.com>
2020-07-08 21:27:34 -0400

    Trabaje en los estilos de slider, del header y las animaciones

--- src/app/components/secciones/header/header.component.ts
+++ src/app/components/secciones/header/header.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'ed-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+
+  @ViewChild('Header') Header;
+
+  anima = 'slide-down'
+  colorHeader: string
 
   constructor() { }
 
-  ngOnInit(): void {
+  // Mostrar u ocultar header y cambiar su color
+  toggleHeader(){
+    let lastScrollTop = 0
+    // al hacer scroll aumenta el valor de sctop
+    let sctop = document.documentElement.scrollTop
+    let st = window.pageXOffset || document.documentElement.scrollTop
+
+    if (st > lastScrollTop){
+      this.anima = 'slide-up'
+      this.colorHeader = 'white'
+    }else if (sctop === 0){
+      this.anima = 'slide-down'
+      this.colorHeader = 'transparent'
+    }
+
+    lastScrollTop = st
   }
+
+  // Detectar scroll
+  @HostListener('window:scroll', [])
+    onWindowScroll(){
+      this.toggleHeader()
+    }
 
 }
