a663e58d6636e25c107ca5b8ac5f75ce9de87d7e
Gabriel Zavando <gabrielzavando@gmail.com>
2020-07-20 21:20:57 -0400

    creacion de tabs y su logica

--- src/app/components/elementos/nosotros/nosotros.component.ts
+++ src/app/components/elementos/nosotros/nosotros.component.ts
@@ -1,17 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'ed-nosotros',
   templateUrl: './nosotros.component.html',
   styleUrls: ['./nosotros.component.scss']
 })
-export class NosotrosComponent implements OnInit {
+export class NosotrosComponent{
+  activo = 1
 
-  constructor() { }
+  constructor( ) { }
 
-  ngOnInit(): void {
+  activar(num: number){
+    switch (num){
+      case 1:
+        this.activo = 1
+        break
+      case 2:
+        this.activo = 2
+        break
+      case 3:
+        this.activo = 3
+        break
+      default:
+        this.activo = 1
+    }
   }
-
-  cambiar(num: any){}
 
 }
